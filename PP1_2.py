b608379a5ee979533c0cf8fb9a12b02b0d01e16e
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-11-04 14:30:22 +0000

    jungkook

--- PP1_2.py
+++ PP1_2.py
@@ -3,23 +3,47 @@
 #Write Assignment code here
 
 def q1():
+    name = "Alex"
+    print(name)
 
 def q2():
+    num1 = 3
+    num2 = 5
+    num3 = 2
+    print(num1 + num2 + num3)
 
 def q3():
+    num1 = 21
+    num2 = 4
+    quotient = num1 // num2
+    print(quotient)
 
 def q4():
+    num1 = 6.3
+    num2 = 3.0
+    quotient = (num1 / num2)
+    print(quotient)
 
 def q5():
+    word = "Hello Word"
+    print(word)
+    word = "Goodbye World"
+    print(word)
 
 def q6():
+    tOrF = True
+    print(tOrF)
 
 def q7():
+    num1 = 7
+    num2 = 2
+    remainder = num1 % num2
+    print(remainder)
 
-q1()
-q2()
-q3()
-q4()
-q5()
-q6()
-q7()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
+#q6()
+#q7()
